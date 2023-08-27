cb407f98dd3f2925902aaea324b15da2df7d09d4
David Kovlak <kovlakdavid4@gmail.com>
2023-08-26 20:12:43 -0700

    Chance to Win

--- MethodsExercise/Exersise1.cs
+++ MethodsExercise/Exersise1.cs
@@ -32,6 +32,41 @@
             Console.WriteLine($"Card Number: {userCardNumber}");
             Console.WriteLine($"EXP: {userEXP}");
             Console.WriteLine($"CVV: {userCVV}");
+
+
+
+
+            Console.WriteLine("--------------------Exercise 2--------------------");
+
+            Console.WriteLine($"Addition: {Sum(5, 3, 8)} ");
+            Console.WriteLine($"Subtraction: {Sub(8, 4, 1, 5, 5)}");
+            Console.WriteLine($"Multiplication: {Multiply(5, 5, 5)}");
+            Console.WriteLine($"Division: {Division(5, 6, 1, 8)}");
         }
+            static int Sum(params int[] numbers)
+            {
+                int result = 0;
+                foreach (int num in numbers) result += num;
+                return result;
+            }
+            static int Sub(params int[] numbers)
+            {
+                int result = numbers[0];
+                for (int bro = 1; bro < numbers.Length; bro++) result -= numbers[bro];
+                return result;
+            }
+            static int Multiply(params int[] numbers)
+            {
+                int result = 1;
+                foreach (int num in numbers) result *= num;
+                return result;
+            }
+            static double Division(params double[] numbers)
+            {
+                double result = numbers[0];
+                for (int bruh = 1; bruh < numbers.Length; bruh++) result /= numbers[bruh];
+                return result;
+
+            }
     }
 }
